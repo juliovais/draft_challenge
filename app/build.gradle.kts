caae30eb44210934d0df32100e8cb8e36c12b83a
juliovais <juliovais@outlook.com>
2023-09-24 12:26:39 +0100

    dependências do room success

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("org.jetbrains.kotlin.android")
     id("kotlin-kapt")
     id("dagger.hilt.android.plugin")
+    id("com.google.devtools.ksp") version "1.8.0-1.0.8"
 }
 
 android {
@@ -71,4 +72,9 @@ dependencies {
 
     // Coil
     implementation("io.coil-kt:coil:1.1.1")
+
+    // Room libraries
+    implementation("androidx.room:room-runtime:2.5.2")
+    ksp("androidx.room:room-compiler:2.5.2")
+    implementation("androidx.room:room-ktx:2.5.2")
 }
